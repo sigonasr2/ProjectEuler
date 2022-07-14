3919984b538136efe324292b888a104b3c983887
sigonasr2, Sig, Sigo <sigonasr2@gmail.com>
2022-07-14 15:08:36 +0000

    Create a prime writer
    Co-authored-by: sigonasr2 <sigonasr2@gmail.com>

--- archives/3/src/main.c
+++ archives/3/src/main.c
@@ -32,6 +32,7 @@ int main(int argc,char**argv) {
             }
             if (primeVal%primes[i]==0) {
                 isPrime=0;
+                break;
             }
         }
         if (isPrime) {
