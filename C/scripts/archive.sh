cbac68a8c48e03dce4a4fafbdf4c3c2b5419ea5a
sigonasr2 <sigonasr2@gmail.com>
2022-07-17 14:19:00 -0500

    Complete
    Co-authored-by: sigonasr2 <sigonasr2@gmail.com>

---
+++ C/scripts/archive.sh
@@ -0,0 +1,11 @@
+#Archives the current project under the applicable program name.
+#C
+if [[ "$#" -eq 2 ]];
+then
+    mkdir archives/$1
+    cp -R src current archives/$1
+    ./sig commit ${*:2}
+else
+    echo "Usage: "
+    echo "  ./sig archive <problem> <commit msg>"
+fi
