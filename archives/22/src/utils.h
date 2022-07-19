bc9a7f513738afb878d4265f0fd02728d00a1c63
sigonasr2, Sig, Sigo <sigonasr2@gmail.com>
2022-07-19 18:56:13 +0000

    Completed 22 with a binary search and insert!
    Co-authored-by: sigonasr2 <sigonasr2@gmail.com>

---
+++ archives/22/src/utils.h
@@ -0,0 +1,14 @@
+#define true 1
+#define false 0
+#define boolean char
+#define byte char
+struct String{
+    int length;
+    char*str;
+};
+struct String add(struct String numb1, struct String numb2);
+struct String mult(struct String numb1, struct String numb2);
+void printLongDoubleArr(int a,int b,long doubleArr[a][b]);
+void printIntDoubleArr(int a,int b,int doubleArr[a][b]);
+struct String createBigNumber(char*numb);
+#define BigNumber(X) createBigNumber(#X)
