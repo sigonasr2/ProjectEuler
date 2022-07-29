581d26b01821bc3eabe8c043db04091379b94367
sigonasr2, Sig, Sigo <sigonasr2@gmail.com>
2022-07-29 15:20:50 +0000

    #32 complete
    Co-authored-by: sigonasr2 <sigonasr2@gmail.com>

---
+++ archives/32/src/utils.h
@@ -0,0 +1,18 @@
+#define true 1
+#define false 0
+#define boolean char
+#define byte char
+struct String{
+    int length;
+    char*str;
+};
+struct String add(struct String numb1, struct String numb2);
+struct String mult(struct String numb1, struct String numb2);
+struct String BigPow(int numb1,int numb2);
+void printLongDoubleArr(int a,int b,long doubleArr[a][b]);
+void printIntDoubleArr(int a,int b,int doubleArr[a][b]);
+struct String createBigNumber(char*numb);
+#define BigNumber(X) createBigNumber(#X)
+boolean isFactor(int*factorList,int numb);
+int*getFactors(int numb);
+boolean equals(struct String str1,struct String str2);
